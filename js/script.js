994925f765601255c4017a00d04eb2765a437275
unknown <williamstobi918@gmail.com>
2024-03-12 21:26:54 +0100

    New project to push to Github

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","Graphics Designer","Product Designer","Content Creator","Kdramalover :)"],
+    typespeed:90,
+    BackSpeed:60,
+    loop:true
+})
